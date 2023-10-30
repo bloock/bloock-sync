27fa04c2b577e0c8b89eba9dcf29eeed0e60bafb
edutomesco <eduard.escoruela@gmail.com>
2023-10-30 01:14:53 -0300

    Add BLOOCK dependencies and http api connections

--- src/cli/sync/mod.rs
+++ src/cli/sync/mod.rs
@@ -1,6 +1,10 @@
 use anchor::Anchor;
+use bloock_smt::tree::SparseMerkleTree;
+use bloock_types::bytes::h256::H256;
+use bloock_merge::hash_algorithms::blake2b::Blake2b;
 use serde::Deserialize;
 use super::http::{bloock_http::BloockHttpClient, Client};
+use bloock_storage::kv::kv_hashmap::HashMap as Database;
 
 mod anchor;
 
@@ -28,6 +32,12 @@ impl SyncService {
     }
 
     pub async fn build_tree(&self) -> String {
+        let mut db = Database::new();
+        let smt: SparseMerkleTree<H256, Database<>, Blake2b> =  match SparseMerkleTree::<H256, Database<>, Blake2b>::new(&mut db, None) {
+            Ok(res) => res,
+            Err(e) => return e.to_string(),
+        };
+
         for index in 1616..=self.max_anchor {
             let base_url = self.http_client.get_api_host();
             let url = format!("{base_url}/core/anchor/{index}");
@@ -47,7 +57,16 @@ impl SyncService {
                 Err(e) => return e.to_string(),
             };
 
+            let bytes_vec: Vec<[u8; 32]> = cids
+            .iter()
+            .map(|hex_string| hex_string_to_bytes(hex_string))
+            .collect();
+
+            smt.add_leaves(bytes_vec).map_err(|e| {
+                return e.to_string()
+            });
             
+             
             println!("CID {:?} inserted to the tree", cids)
         }
 
@@ -56,4 +75,16 @@ impl SyncService {
     }
 
     
+}
+
+fn hex_string_to_bytes(hex_string: &str) -> [u8; 32] {
+    let mut result = [0u8; 32];
+    let bytes = hex::decode(hex_string).expect("Invalid hexadecimal string");
+
+    // Ensure bytes has exactly 32 bytes
+    assert_eq!(bytes.len(), 32);
+
+    // Copy bytes into result array
+    result.copy_from_slice(&bytes);
+    result
 }
